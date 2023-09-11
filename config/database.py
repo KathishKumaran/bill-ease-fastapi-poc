cedd4d4cd0fa3d2a9ea2774cbda832ab4695926a
kathish <kathish@yavar.in>
2023-09-11 10:54:03 +0530

    folder structure

--- config/database.py
+++ config/database.py
@@ -9,5 +9,4 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-# Session = sessionmaker(bind=engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
